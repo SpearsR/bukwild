807984df3737f0b5cd80475dd5af51364299269f
SpearsR <18rspears@gmail.com>
2021-03-17 13:43:20 -0600

    Needs some polishing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Bukwild from './Bukwild';
+import content from './content.json';
+import logo from './abc_logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            index : 0
+        }
+    }
+
+    render(){
+        const backgroundUrl = require(`./backgrounds/${content.pages[this.state.index].blocks[0].background}`);
+        return(
+            <div style={{
+                backgroundImage: `url(${backgroundUrl.default})`
+                }} className="main-container">
+                <div className="top-ui">
+                    <div className="logo-contact-box">
+                        <img id="logo" src={logo} alt="abc logo for website"/>
+                        <div id="contact-button" >Contact Us</div>
+                    </div>
+                    <div onClick={() => this.setState({index: 0})} className="nav-link">Industries</div>
+                    <div onClick={() => this.setState({index: 1})} className="nav-link">Services</div>
+                    <div onClick={() => this.setState({index: 2})} className="nav-link">About Us</div>
+                </div>
+                <Bukwild index={this.state.index} />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Bukwild.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import content from './content.json';
+import arrow from './right-arrow.svg';
+class Bukwild extends React.Component{
+
+    componentDidMount(){
+        document.getElementById('test').className += " fade-in";
+        const animated = document.querySelector('.fade-in');
+        animated.addEventListener('animationend', () => {
+            document.getElementById('test').classList.remove('fade-in');
+          });
+    }
+    
+    componentDidUpdate(){
+        document.getElementById('test').className += " fade-in";
+        const animated = document.querySelector('.fade-in');
+        animated.addEventListener('animationend', () => {
+            document.getElementById('test').classList.remove('fade-in');
+        });
+    }
+    
+    componentWillUnmount(){
+        document.getElementById('test').classList.remove('fade-in');
+    }
+
+
+    render(){
+     return(
+            <>  
+                {/*Here is the middle headline content*/}
+                <div id="test" className="headings">
+                    <div className="headline">{content.pages[this.props.index].blocks[0].headline}</div>
+                    <div className="subhead">{content.pages[this.props.index].blocks[0].subhead}</div>
+                </div>
+
+                {/*Here is the CTA*/}
+                <div className="cta-box">
+                    <div className="cta-text">{content.pages[this.props.index].blocks[0].cta}</div>
+                    <div className="lets-talk-button">
+                        <span className="LETS-TALK">LET'S TALK.</span>
+                        <img src={arrow} alt="arrow icon"/>
+                    </div>
+                </div>
+            </>
+        );
+    }
+}
+
+export default Bukwild;
+
